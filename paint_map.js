9cb4dbb1d0e7c845eac5cd261ab2049a214cfbf3
bernardo <bernardo@celere-ce.com.br>
2020-07-27 17:23:32 -0300

    minor fixes

--- paint_map.js
+++ paint_map.js
@@ -83,29 +83,8 @@ $(document).ready(function () {
 
     // https://docs.google.com/forms/d/e/1FAIpQLSfwqYsWo5Kbv8sOqOcOv0hzlItBQiE1svZ8Y5NTdHmi8EVHYA/viewform?usp=sf_link
 
-    $('#my-form').submit(function(e) {
-        //prevent the form from submiting so we can post to the google form
-        e.preventDefault();
-        //AJAX request
-        $.ajax({
-            url: 'https://docs.google.com/forms/d/e/1FAIpQLSfwqYsWo5Kbv8sOqOcOv0hzlItBQiE1svZ8Y5NTdHmi8EVHYA/formResponse?',     //The public Google Form url, but replace /view with /formResponse
-            data: 'teste', // $('#my-form').serialize(), //Nifty jquery function that gets all the input data
-            type: 'POST', //tells ajax to post the data to the url
-            dataType: "json", //the standard data type for most ajax requests
-            statusCode: { //the status code from the POST request
-                0: function(data) { //0 is when Google gives a CORS error, don't worry it went through
-                    //success
-                    $('#form-success').text('hooray!');
-                },
-                200: function(data) {//200 is a success code. it went through!
-                    //success
-                    $('#form-success').text('hooray!');
-                },
-                403: function(data) {//403 is when something went wrong and the submission didn't go through
-                    //error
-                    alert('Oh no! something went wrong. we should check our code to make sure everything matches with Google');
-                }
-            }
-        });
-    });
+    // $('#my-form').submit(function(e) {
+    //     e.preventDefault();
+    //
+    // });
 })
