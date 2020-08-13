d9af1d9a839076492f32e28000ca7a362fbcdd86
Bernardo <bernardovassis@fisica.if.uff.br>
2020-08-13 12:29:30 -0300

    now with back end connections!

--- paint_map.js
+++ paint_map.js
@@ -1,7 +1,13 @@
 $(document).ready(function () {
-    var mapa = document.getElementById("palacio-map");
-    var caixa_legenda = document.getElementById("legenda-div")
-    var botao_ok = document.getElementById('legenda-form-input-botao')
+    // let url = "http://127.0.0.1:5000/pesquisa_0";
+    let url = 'https://palacio-e-a-cidade.herokuapp.com/pesquisa_0'
+    let mapa = document.getElementById("palacio-map");
+    let caixa_legenda = document.getElementById("legenda-div")
+    let botao_ok = document.getElementById('legenda-form-input-botao')
+    let botao_enviar = document.getElementById('botao-enviar')
+    var name = ''
+    var tel = ''
+    var data = {}
     var categoria = null
     var lista_espacos = document.querySelectorAll("path");
     var lista_legendas = document.querySelectorAll(".legenda")
@@ -12,17 +18,17 @@ $(document).ready(function () {
 
 
     mapa.addEventListener("click",
-        function(e){
+        function (e) {
             var espaco = e.target;
 
             for (var i = 0; i < lista_espacos.length; i++) {
-                    lista_espacos[i].classList.remove("active");
+                lista_espacos[i].classList.remove("active");
             }
 
             for (var i = 0; i < lista_legendas.length; i++) {
                 lista_legendas[i].classList.remove("active");
 
-                if(espaco.classList.contains(lista_legendas[i].getAttribute("categoria"))){
+                if (espaco.classList.contains(lista_legendas[i].getAttribute("categoria"))) {
                     lista_legendas[i].classList.add(('active'))
                 } else {
                     lista_legendas[i].classList.remove(('active'))
@@ -35,45 +41,45 @@ $(document).ready(function () {
             }
         }
     )
-   caixa_legenda.addEventListener(
-       "click",
-       function (e) {
-           var legenda = e.target.parentNode;
-           var espaco = null
-           var categoria = legenda.getAttribute("categoria")
+    caixa_legenda.addEventListener(
+        "click",
+        function (e) {
+            var legenda = e.target.parentNode;
+            var espaco = null
+            var categoria = legenda.getAttribute("categoria")
 
-           for (var i = 0; i < lista_espacos.length; i++) {
-               if(lista_espacos[i].classList.contains("active")) {
-                   lista_espacos[i].classList.remove("active");
-                   espaco = lista_espacos[i]
-               }
-           }
-           if (espaco) {
-               var classList = espaco.classList;
-               if(!categoria){
-                   for (var i = 0; i < lista_legendas.length; i++) {
-                       lista_legendas[i].classList.remove("active");
-                   }
-                   while (classList.length > 0) {
-                       classList.remove(classList.item(0));
-                   }
-               }else if(classList.contains(categoria)) {
-                   espaco.classList.remove(categoria)
-                   legenda.classList.remove("active")
-               } else {
-                   espaco.classList.add(categoria)
-                   legenda.classList.add("active")
-               }
-           }
-           espaco.classList.add("active")
-       }
-   )
+            for (var i = 0; i < lista_espacos.length; i++) {
+                if (lista_espacos[i].classList.contains("active")) {
+                    lista_espacos[i].classList.remove("active");
+                    espaco = lista_espacos[i]
+                }
+            }
+            if (espaco) {
+                var classList = espaco.classList;
+                if (!categoria) {
+                    for (var i = 0; i < lista_legendas.length; i++) {
+                        lista_legendas[i].classList.remove("active");
+                    }
+                    while (classList.length > 0) {
+                        classList.remove(classList.item(0));
+                    }
+                } else if (classList.contains(categoria)) {
+                    espaco.classList.remove(categoria)
+                    legenda.classList.remove("active")
+                } else {
+                    espaco.classList.add(categoria)
+                    legenda.classList.add("active")
+                }
+            }
+            espaco.classList.add("active")
+        }
+    )
 
-    botao_ok.addEventListener("click", function ()  {
+    botao_ok.addEventListener("click", function () {
         var espaco = null
         if (this.value.length > 1) {
             for (var i = 0; i < lista_espacos.length; i++) {
-                if(lista_espacos[i].classList.contains("active")) {
+                if (lista_espacos[i].classList.contains("active")) {
                     espaco = lista_espacos[i]
                 }
             }
@@ -81,10 +87,23 @@ $(document).ready(function () {
         }
     });
 
-    // https://docs.google.com/forms/d/e/1FAIpQLSfwqYsWo5Kbv8sOqOcOv0hzlItBQiE1svZ8Y5NTdHmi8EVHYA/viewform?usp=sf_link
+    botao_enviar.addEventListener("click", function () {
+        name = document.querySelector('#nome');
+        tel = document.querySelector('#tel');
+        mapa_svg = serializer.serializeToString(mapa)
+        data = {"name": name.value, "tel": tel.value, 'mapa': mapa_svg}
 
-    // $('#my-form').submit(function(e) {
-    //     e.preventDefault();
-    //
-    // });
-})
+        console.log(data)
+
+        let response = fetch(url, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(data)
+        })
+    })
+});
+
+
